c881a0e0a3bb02e2f08456f93709211a605d59d5
cjsaudgns <cjsaudgns20@naver.com>
2020-05-27 10:53:29 +0900

    Day_2 업데이트함.

---
+++ pandas.py
@@ -0,0 +1,12 @@
+temp = pd.read_csv('data_1. csv')
+print(temp)
+
+print(temp['MON'])
+
+
+temp['T'] = 0
+print(temp)
+
+import matplotlib.pyplot as plt
+plt.plot(temp['A']) dashes = [6, 2]
+Plt.show()
